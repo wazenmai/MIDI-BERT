9047a9277f186849e8fc7289e50dd64e48f27d12
Sophia Chou <47291963+sophia1488@users.noreply.github.com>
2022-04-29 10:24:00 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -4,11 +4,11 @@ export PYTHONPATH="."
 python3 MidiBERT/eval.py --task=melody --cpu
 
 # velocity
-#python3 MidiBERT/eval.py --task=velociy --cpu
+python3 MidiBERT/eval.py --task=velociy --cpu
 
 # composer
-#python3 MidiBERT/eval.py --task=composer --cpu
+python3 MidiBERT/eval.py --task=composer --cpu
 
 # emotion
-#python3 MidiBERT/eval.py --task=emotion --cpu
+python3 MidiBERT/eval.py --task=emotion --cpu
 
