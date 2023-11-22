59c8239d9f0701cac2456d693bbdb0dd4d9e229d
Yi-Hui Chou <yihuic@patient.lti.cs.cmu.edu>
2023-11-21 19:27:32 -0500

    finetune scripts

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,14 +1,15 @@
 export PYTHONPATH="."
+# melody, its output folder name will be {task}_{tag}
 
-# melody, its output folder name will be {task}_{name}
-python3 MidiBERT/finetune.py --task=melody --name=default
+pretrain_ckpt=MidiBERT/result/pretrain_CP/test/model_best.ckpt
+python3 MidiBERT/finetune.py --task=melody --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
 # velocity
-python3 MidiBERT/finetune.py --task=velociy --name=default
+python3 MidiBERT/finetune.py --task=velociy --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
 # composer
-python3 MidiBERT/finetune.py --task=composer --name=default
+python3 MidiBERT/finetune.py --task=composer --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
 # emotion
-python3 MidiBERT/finetune.py --task=emotion --name=default
+python3 MidiBERT/finetune.py --task=emotion --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
