d01d6209a244becd5278367f17ccfd86b14f742c
Yi-Hui Chou <yihuic@patient.lti.cs.cmu.edu>
2023-11-21 19:45:11 -0500

    fix typo

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -2,10 +2,10 @@ export PYTHONPATH="."
 # melody, its output folder name will be {task}_{tag}
 
 pretrain_ckpt=MidiBERT/result/pretrain_CP/test/model_best.ckpt
-python3 MidiBERT/finetune.py --task=melody --tag=default --ckpt=$pretrain_ckpt --repr=CP
+#python3 MidiBERT/finetune.py --task=melody --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
 # velocity
-python3 MidiBERT/finetune.py --task=velociy --tag=default --ckpt=$pretrain_ckpt --repr=CP
+python3 MidiBERT/finetune.py --task=velocity --tag=default --ckpt=$pretrain_ckpt --repr=CP
 
 # composer
 python3 MidiBERT/finetune.py --task=composer --tag=default --ckpt=$pretrain_ckpt --repr=CP
